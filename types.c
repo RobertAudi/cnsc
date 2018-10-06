92a0f493341190bb226fade8b43853b4bf6c7937
Robert Audi <robert.audii@gmail.com>
2018-10-06 21:33:03 +0200

    style: reorder the types

--- types.c
+++ types.c
@@ -3,16 +3,14 @@
 #include "cnsc.h"
 
 const struct type_t TYPES[] = {
-    { "feat", "a new feature" },
-    { "fix", "a bug fix" },
-    { "docs", "documentation only changes" },
-    { "style", "changes that do not affect the meaning of the code" },
-    { "refactor", "a code change that neither fixes a bug nor adds a new "
-                  "feature" },
-    { "perf", "a code change that improves performance" },
-    { "test", "adds missing tests" },
-    { "db", "changes the database" },
-    { "chore", "changes to the build process or auxiliary tools or libraries "
-               "such as documentation generation" },
+    { "feat",     "a new feature" },
+    { "fix",      "a bug fix" },
+    { "refactor", "a code change that neither fixes a bug nor adds a new feature" },
+    { "perf",     "a code change that improves performance" },
+    { "style",    "changes that do not affect the meaning of the code" },
+    { "docs",     "documentation only changes" },
+    { "test",     "adds missing tests" },
+    { "db",       "changes the database" },
+    { "chore",    "changes to the build process or auxiliary tools or libraries such as documentation generation" },
     { NULL, NULL }
 };
